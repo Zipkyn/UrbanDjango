4845e41827490edd65380e2daa44fe2128f5323b
Zipkyn <v.t.s.782003@gmail.com>
2025-01-20 17:04:15 +0300

    Исправлен конфликт в settings.py

--- UrbanDjango/UrbanDjango/settings.py
+++ UrbanDjango/UrbanDjango/settings.py
@@ -122,7 +122,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-=======
 """
 Django settings for UrbanDjango project.
 
